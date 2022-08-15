47d26544737c49949de420b3e6cd0e3603df060f
Roman Sharkovich <roman.sharkovich@solbeg.com>
2022-08-15 22:40:39 +0200

    mth to m

--- ReversedLinkedList/Program.cs
+++ ReversedLinkedList/Program.cs
@@ -6,6 +6,19 @@ namespace ReversedLinkedList
     {
         public static void Main(String[] args)
         {
+            // mth elemnt to m
+            LinkedList linkedList = new LinkedList();
+
+            linkedList.push(4);
+            linkedList.push(3);
+            linkedList.push(2);
+            linkedList.push(1);
+            linkedList.push(0);
+
+            Console.WriteLine("Element at index 3 is " + linkedList.GetMth(3));
+            Console.WriteLine("Element at index 4 is " + linkedList.GetMth(4));
+
+            // recursion
             var recursion = new ListRecursion();
 
             recursion.push(13);

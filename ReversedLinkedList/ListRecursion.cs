fe2d0a8cfd796f176fb1abac25393018c15a6e2f
Roman Sharkovich <roman.sharkovich@solbeg.com>
2022-08-15 20:49:47 +0200

    reversed linked list using recursion

---
+++ ReversedLinkedList/ListRecursion.cs
@@ -0,0 +1,39 @@
+﻿namespace ReversedLinkedList
+{
+    class ListRecursion
+    {
+        public Node headItem;
+
+        public Node reverse(Node headItem)
+        {
+            if (headItem == null ||
+                headItem.next == null)
+                return headItem;
+
+            Node rest = reverse(headItem.next);
+            headItem.next.next = headItem;
+
+            headItem.next = null;
+
+            return rest;
+        }
+
+        public void print()
+        {
+            Node temp = headItem;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+
+        public void push(int data)
+        {
+            Node temp = new Node(data);
+            temp.next = headItem;
+            headItem = temp;
+        }
+    }
+}
---
+++ ReversedLinkedList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ReversedLinkedList
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            var recursion = new ListRecursion();
+
+            recursion.push(13);
+            recursion.push(23);
+            recursion.push(35);
+            recursion.push(53);
+            recursion.push(54);
+
+            Console.WriteLine("linked list before: ");
+            recursion.print();
+            recursion.headItem = recursion.reverse(recursion.headItem);
+            Console.WriteLine("linked list after: ");
+            recursion.print();
+        }
+    }
+}

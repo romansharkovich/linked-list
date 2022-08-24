c2667623d8defb769699ca372211d380b810189b
Roman Sharkovich <roman.sharkovich@solbeg.com>
2022-08-25 00:09:42 +0200

    add TreeNode

---
+++ LinkedList/TreeNode.cs
@@ -0,0 +1,26 @@
+﻿namespace LinkedListSpace
+{
+    public class TreeNode
+    {
+        public int data { get; set; }
+
+        private TreeNode _left;
+        private TreeNode _right;
+        public TreeNode Left
+        {
+            get { return _left; }
+            set { _left = value; }
+        }
+        public TreeNode Right
+        {
+            get { return _right; }
+            set { _right = value; }
+        }
+
+        public TreeNode(int item)
+        {
+            data = item;
+            _left = _right = null;
+        }
+    }
+}
